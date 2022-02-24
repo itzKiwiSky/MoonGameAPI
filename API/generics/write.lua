f3e57753b491874fa05502b6eb7bb49107ea0706
unknown <Doge2Dev>
2022-02-23 21:07:37 -0300

    fixed issues

--- API/generics/write.lua
+++
@@ -1,7 +0,0 @@
-powerprint = {}
-
-function powerprint.write(text, color1, color2)
-   os.execute("powershell write-host " .. text .. " -backgroundColor " .. color1 .. " -foregroundColor " .. color2)
-end
-
-return powerprint
